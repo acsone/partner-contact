980916b54e4af80560bc337b5e9a8ca6ac23623b
Denis Roussel <denis.roussel@acsone.eu>
2016-08-30 16:11:12 +0200

    As complement, allow to store also the GS1 GCP

--- partner_identification_gln/models/partner.py
+++ partner_identification_gln/models/partner.py
@@ -20,9 +20,32 @@ class ResPartnerIdCategory(models.Model):
         except InvalidChecksum:
             return True
 
+        cat = self.env.ref('partner_identification_gln.'
+                           'partner_identification_gln_number_category').id
         num_obj = self.env['res.partner.id_number']
-        duplicate_gln = num_obj.search([('category_id', '=',
-                                         id_number.category_id.id),
+        duplicate_gln = num_obj.search([('category_id', '=', cat),
+                                        ('name', '=', id_number.name),
+                                        ('name', '!=', False),
+                                        ('id', '!=', id_number.id)])
+
+        if duplicate_gln:
+            return True
+
+        return False
+
+    @api.multi
+    def validate_res_partner_gcp(self, id_number):
+        self.ensure_one()
+        if not id_number:
+            return False
+
+        if len(id_number.name) < 1 or len(id_number.name) > 12:
+            return True
+
+        cat = self.env.ref('partner_identification_gln.'
+                           'partner_identification_gcp_number_category').id
+        num_obj = self.env['res.partner.id_number']
+        duplicate_gln = num_obj.search([('category_id', '=', cat),
                                         ('name', '=', id_number.name),
                                         ('name', '!=', False),
                                         ('id', '!=', id_number.id)])
