397f5c74f73e0489a6ab6b8854d3bafafe484213
Denis Roussel <denis.roussel@acsone.eu>
2016-08-30 16:11:12 +0200

    [ADD] As complement, allow to store also the GS1 GCP

--- partner_identification_gln/tests/test_gln.py
+++ partner_identification_gln/tests/test_gln.py
@@ -13,8 +13,11 @@ class TestGLN(TransactionCase):
         self.partner2 = self.env['res.partner'].create({'name': 'TestGLN2'})
         pc = self.env.ref('partner_identification_gln.'
                           'partner_identification_gln_number_category')
-
         self.partner_id_category = pc
+
+        pc_gcp = self.env.ref('partner_identification_gln.'
+                              'partner_identification_gcp_number_category')
+        self.partner_id_gcp_category = pc_gcp
 
     def test_gln(self):
         # Good GLN
@@ -37,6 +40,31 @@ class TestGLN(TransactionCase):
         # Bad GLN
         vals = {'name': '5450534001716',
                 'category_id': self.partner_id_category.id
+                }
+        with self.assertRaises(ValidationError):
+            self.partner.write({'id_numbers': [(0, 0, vals)]})
+
+    def test_gcp(self):
+        # Good GLN
+        vals = {'name': '545053',
+                'category_id': self.partner_id_gcp_category.id
+                }
+        self.partner.write({'id_numbers': [(0, 0, vals)]})
+        id_number = self.partner.id_numbers[0]
+
+        self.assertEqual(id_number.name, '545053')
+
+        # Duplicate GLN
+        vals = {'name': '545053',
+                'category_id': self.partner_id_gcp_category.id
+                }
+
+        with self.assertRaises(ValidationError):
+            self.partner2.write({'id_numbers': [(0, 0, vals)]})
+
+        # Bad GLN
+        vals = {'name': '5450534001716',
+                'category_id': self.partner_id_gcp_category.id
                 }
         with self.assertRaises(ValidationError):
             self.partner.write({'id_numbers': [(0, 0, vals)]})
